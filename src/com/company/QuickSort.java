740614850cb5305ddee028ea5991d7190cab2a99
Andriy <dreamcaster@ukr.net>
2014-05-15 13:04:13 +0300

    Test

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -4,7 +4,11 @@ package com.company;
  * Created by Nadya on 15.05.2014.
  */
 public class QuickSort {
+    int answer;
     public int answer(int[] tall){
 
+        return answer;
+
     }
+    private void Sort()
 }
