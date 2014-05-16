d31bec2f5086e071cb600472b91fd9ca932e8892
andrey-- <ngener42>
2014-05-16 12:51:31 +0300

    Final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
+
 import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         FileOperation reader=new FileOperation();
-        int [] tall =new int[100000];
-        double inversions=0;
+        int [] tall =new int[10000];
+        int answer;
         tall=reader.readFromFile();
+        QuickSort quick=new QuickSort();
+        answer=quick.answer(tall);
+
+        for (int i=0;i<tall.length;i++) {
+            if (tall[i] != i + 1)
+                System.out.println("ACHTUNG!!!");
+            System.out.println(tall[i]);
+        }
+        System.out.println(answer);
 
     }
 }
--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -7,8 +7,51 @@ public class QuickSort {
     int answer;
     public int answer(int[] tall){
 
+        sort(tall, 0, tall.length);
         return answer;
+    }
+    private void swap(int[]tall, int i, int j){
+        int temp;
+        temp=tall[i];
+        tall[i]=tall[j];
+        tall[j]=temp;
+    }
+    private int median(int[] tall, int l, int r){
+        int med;
+        med = (r - l + 1) % 2 != 0 ? (l + r+1 ) / 2 : (l + r +1) / 2- 1;
+        if((tall[l]>=tall[med]&&tall[l]<=tall[r])||(tall[l]<=tall[med]&&tall[l]>=tall[r]))
+            return l;
+        if((tall[med]>=tall[l]&&tall[med]<=tall[r])||(tall[med]<=tall[l]&&tall[med]>=tall[r]))
+            return med;
+        return r;
+    }
+    private void sort(int[]tall, int l, int r){
+        int med;
+        int i;
+        int j;
+        int p;
+
+        if (r-l>1) {
+            answer+=r-l-1;
+
+//          med = median(tall, l, r-1);
+//          swap(tall, l, r-1);
+//          swap(tall, l, med);
+
+
+            p=tall[l];
+
+            i=l+1;
+            for (j=l+1;j<r;j++){
+                if (tall[j]<p) {
+                    swap(tall, i, j);
+                    i++;
+                }
+            }
+            swap(tall, l, i-1);
+            sort(tall, l, i-1);
+            sort(tall, i, r);
+        }
 
     }
-    private void Sort()
 }
